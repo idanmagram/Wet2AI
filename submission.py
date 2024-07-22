727a30df1e184f5970828a8f76c52dbe7285018b
idanmagram <idan.magram@gmail.com>
2024-07-22 22:49:03 +0300

    implemented AgentMinimax

--- submission.py
+++ submission.py
@@ -10,38 +10,7 @@ import time
 
 
 # TODO: section a : 3
-'''
-def smart_heuristic(env: WarehouseEnv, robot_id: int):
-    robot = env.get_robot(robot_id)
-    if robot.battery <= 3 and robot_id == 1:
-        x = 5
-    other_robot = env.get_robot((robot_id + 1) % 2)
-    pos_packages = [package.position for package in env.packages if package.on_board]
-    robot1_to_package = 10
-    robot2_to_package = 10
-    robot1_to_dest = 10
-    robot2_to_dest = 10
-    pick_up = 0
 
-    if robot.package is None:
-        robot1_to_package = min([manhattan_distance(robot.position, pos) for pos in pos_packages])
-    if other_robot.package is None:
-        robot2_to_package = min([manhattan_distance(other_robot.position, pos) for pos in pos_packages])
-
-    if robot.package is not None:
-        robot1_to_dest = manhattan_distance(robot.position, robot.package.destination)
-        pick_up = 10
-
-    if other_robot.package is not None:
-        robot2_to_dest = manhattan_distance(robot.position, robot.package.destination)
-
-    print("robot1_to_dest ", robot1_to_dest, "robot1_to_package ", robot1_to_package, "sum ", "pick_up ", pick_up,
-          (robot.battery - other_robot.battery) + (robot.credit - other_robot.credit) * 1000 + \
-          (10 / (robot1_to_dest + 0.001)) + (10 / (robot1_to_package + 0.001)) + pick_up
-          )
-    return (robot.battery - other_robot.battery) + (robot.credit - other_robot.credit)*1000 + \
-            (10/(robot1_to_dest+0.001)) + (10/(robot1_to_package+0.001)) + pick_up - (robot1_to_dest-robot2_to_dest)
-'''
 
 def smart_heuristic_aux(env: WarehouseEnv, robot_id: int):
     robot = env.get_robot(robot_id)
@@ -71,36 +40,22 @@ class AgentGreedyImproved(AgentGreedy):
 
 class AgentMinimax(Agent):
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        '''
-        last_action = None
-        try:
-            # Pass the function as a callable object to func_timeout
-            action = func_timeout(time_limit, lambda: self.iterative_run_step(env, agent_id))
-            last_action = action
-        except FunctionTimedOut:
-            print("Processing took too long and was aborted.")
-        return last_action
-        '''
+        finish_time = time.time() + time_limit - 0.1
         depth = 0
-        start_time = time.time()
-        action = 'move north'
-        while (time.time() - start_time) < time_limit:
-            value, action = self.rb_min_max(env, agent_id, agent_id, depth, 'move north')
-            depth += 1
-        return action
-    '''
-    def iterative_run_step(self, env: WarehouseEnv, agent_id):
-        depth = 0
-        action = "move north"
-     
-        
+        action = None
         try:
-            value, action = self.rb_min_max(env, agent_id, agent_id, depth, 'move north')
-            depth += 1
+            while True:
+                if time.time() >= finish_time:
+                    raise Exception("Agent used too much time!")
+                _, action = self.rb_min_max(env, agent_id, agent_id, depth, action, finish_time)
+                depth += 1
+        except:
             return action
-    '''
 
-    def rb_min_max(self, env: WarehouseEnv, my_agent_id, curr_agent_id, depth, act):
+
+    def rb_min_max(self, env: WarehouseEnv, my_agent_id, curr_agent_id, depth, act, finish_time):
+        if time.time() >= finish_time:
+            raise Exception("Agent used too much time!")
         if env.done() or depth == 0:
             return smart_heuristic(env, my_agent_id), act
 
@@ -110,7 +65,7 @@ class AgentMinimax(Agent):
             curr_max = -math.inf
             op_max = ""
             for child, op in zip(children, operators):
-                v, _ = self.rb_min_max(child, my_agent_id, (curr_agent_id + 1) % 2, depth - 1, op)
+                v, _ = self.rb_min_max(child, my_agent_id, (curr_agent_id + 1) % 2, depth - 1, op, finish_time)
                 if v > curr_max:
                     curr_max = v
                     op_max = op
@@ -120,7 +75,7 @@ class AgentMinimax(Agent):
             curr_min = math.inf
             op_min = ""
             for child, op in zip(children, operators):
-                v, _ = self.rb_min_max(child, my_agent_id, (curr_agent_id + 1) % 2, depth - 1, op)
+                v, _ = self.rb_min_max(child, my_agent_id, (curr_agent_id + 1) % 2, depth - 1, op, finish_time)
                 if v < curr_min:
                     curr_min = v
                     op_min = op
